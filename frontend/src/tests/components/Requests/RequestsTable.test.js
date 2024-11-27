6883f3e703e0916c648070a53c22fc0bdbb61691
Ponmo <ponmonw@gmail.com>
2024-11-26 18:40:49 -0800

    prettier fix

--- frontend/src/tests/components/Requests/RequestsTable.test.js
+++ frontend/src/tests/components/Requests/RequestsTable.test.js
@@ -11,7 +11,7 @@ describe("RequestsTable tests", () => {
     render(
       <RequestsTable
         requests={recommendationRequestFixtures.oneRecommendationRequest}
-      />
+      />,
     );
   });
 
@@ -19,7 +19,7 @@ describe("RequestsTable tests", () => {
     render(
       <RequestsTable
         requests={recommendationRequestFixtures.threeRecommendationRequests}
-      />
+      />,
     );
 
     const expectedHeaders = [
@@ -57,16 +57,16 @@ describe("RequestsTable tests", () => {
     });
 
     expect(screen.getByTestId(`${testId}-cell-row-0-col-id`)).toHaveTextContent(
-      "2"
+      "2",
     );
     expect(
-      screen.getByTestId(`${testId}-cell-row-0-col-requesterId`)
+      screen.getByTestId(`${testId}-cell-row-0-col-requesterId`),
     ).toHaveTextContent("7");
     expect(screen.getByTestId(`${testId}-cell-row-1-col-id`)).toHaveTextContent(
-      "3"
+      "3",
     );
     expect(
-      screen.getByTestId(`${testId}-cell-row-1-col-professorId`)
+      screen.getByTestId(`${testId}-cell-row-1-col-professorId`),
     ).toHaveTextContent("1");
   });
 });
